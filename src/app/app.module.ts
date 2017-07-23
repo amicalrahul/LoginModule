8db649a13249592ae4c9b02674e0db63c4a862ec
Rahul Sabharwal <amicalrahul@gmail.com>
2017-07-23 17:26:08 +1000

    Added Auth Service and Login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
+import './rxjs-extensions';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationService } from './services/authentication.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { AuthenticationService } from '../services/authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  @Output() postLogin = new EventEmitter();
+  @Input() showRememberMe: boolean;
+
+  private loginForm: any = null;
+  constructor(private fb: FormBuilder, private authenticationService: AuthenticationService) {
+    this.loginForm = fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      rememberMe: [false]
+    });
+  }
+  ngOnInit() {
+    if (!this.showRememberMe) {
+      this.showRememberMe = false;
+    }
+  }
+  performLogin() {
+    this.authenticationService.login(this.loginForm.value.username, this.loginForm.value.password)
+    .subscribe(loginResponse => {
+            this.postLogin.emit(loginResponse);
+      });
+  }
+}
